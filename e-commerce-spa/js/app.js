a9e00d2d083abc540c321235394dc0d9e4988c0b
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2025-11-05 15:46:29 +0100

    update

--- e-commerce-spa/js/app.js
+++ e-commerce-spa/js/app.js
@@ -3,5 +3,5 @@ function changePage(idPage) {
   // pages.forEach((p) => p.classList.add("hidden"));
 
   document.querySelectorAll(".page").forEach((p) => p.classList.add("hidden"));
-  document.querySelector("#" + idPage).classList.remove("hidden");
+  document.querySelector(idPage).classList.remove("hidden");
 }
