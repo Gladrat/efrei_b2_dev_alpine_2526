e041702b3b747b78afefcd8d73c432f915410f91
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2025-11-05 15:16:41 +0100

    update

--- e-commerce-spa/js/app.js
+++ e-commerce-spa/js/app.js
@@ -1,5 +1,3 @@
-// La navigation est composée de liens ou de boutons
-  // Page 01
-  // Page 02
-  // Cart
-// Le clic sur un bouton affiche la div correspondante et masque les autres
+function changePage(page) {
+  // Le clic sur un bouton affiche la div correspondante et masque les autres
+}
