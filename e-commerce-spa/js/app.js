f1c16e3318d20bc413c03f2fda154adca6a6b555
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2025-11-05 18:07:55 +0100

    update

--- e-commerce-spa/js/app.js
+++ e-commerce-spa/js/app.js
@@ -11,3 +11,22 @@ document.addEventListener("DOMContentLoaded", () => {
   location.hash = "#product01"
   changePage(location.hash);
 });
+
+// function changePage(idPage) {
+//   document.querySelectorAll(".page").forEach((p) => p.classList.add("hidden"));
+//   document.querySelector(idPage).classList.remove("hidden");
+
+//   if (idPage === "#product01") {
+//     document.querySelector("#product01").innerHTML = `
+//       <h1>Product 01</h1>
+//       <button class="add" id="product_01">Add to cart</button>`;
+//   } else if (idPage === "#product02") {
+//     document.querySelector("#product02").innerHTML = `
+//       <h1>Product 02</h1>
+//       <button class="add" id="product_02">Add to cart</button>`;
+//   } else if (idPage === "#cart") {
+//     document.querySelector("#cart").innerHTML = `
+//       <h1>Cart</h1>
+//       <button class="show-cart">Show Cart</button>`;
+//   }
+// }   
---
+++ jsx-homemade/app.js
@@ -0,0 +1,6 @@
+// Composant "Button"
+// Fonction JavaScript
+  // Retourne élément HTML : document.createElement
+
+const root = document.querySelector("root");
+// root.appendChild()
